505912f3a2d989d6b8bdc4ba1fde5413d41631a1
Dima Lipin <dimich3d@ya.ru>
2023-01-31 19:28:10 +0100

    basic deploy

---
+++ wheel-of-learning-frontend/index.js
@@ -0,0 +1,11 @@
+function getTasks() {
+    fetch('/api/tasks')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
---
+++ wheel_of_learning_backend/deploy.py
@@ -0,0 +1,104 @@
+import remoto
+import shlex
+
+
+class Tool:
+
+    @staticmethod
+    def exist_cmd() -> str:
+        return ""
+
+    @staticmethod
+    def install_cmd() -> str:
+        return ""
+
+
+class Path:
+
+    PATH = "/root/.local/bin"
+
+    @staticmethod
+    def exist_cmd() -> str:
+        return """python3 -c 'import os; exit(1) if "/root/.local/bin" not in os.environ["PATH"] else exit(0)'"""
+
+    @staticmethod
+    def install_cmd() -> str:
+        cmd1 = """echo "export PATH=/root/.local/bin:$PATH" >> /root/myrc"""
+        cmd2 = """echo "export PATH=/root/.local/bin:$PATH" >> /root/.bashrc"""
+        cmd3 = """echo "export PATH=/root/.cargo/bin:$PATH" >> /root/myrc"""
+        return f"""{cmd1} && {cmd2} && {cmd3}"""
+
+
+class Poetry(Tool):
+
+    @staticmethod
+    def exist_cmd() -> str:
+        return "poetry --version"
+
+    @staticmethod
+    def install_cmd() -> str:
+        return "curl -sSL https://install.python-poetry.org | python3"
+
+class Rust(Tool):
+    @staticmethod
+    def exist_cmd() -> str:
+        return "cargo --version"
+
+    @staticmethod
+    def install_cmd() -> str:
+        return "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y"
+
+class Just(Tool):
+    @staticmethod
+    def exist_cmd() -> str:
+        return "just --version"
+
+    @staticmethod
+    def install_cmd() -> str:
+        return "cargo install just"
+
+class Repo(Tool):
+    @staticmethod
+    def exist_cmd() -> str:
+        return "cd wheel-of-learning && git fetch --all && git reset --hard origin/master"
+
+    @staticmethod
+    def install_cmd() -> str:
+        return "git clone https://github.com/festeh/wheel-of-learning.git"
+
+def prepare_cmd(cmd):
+    cmd = cmd.strip()
+    # cmd = cmd.replace('$', '\\$')
+    # cmd = cmd.replace('`', '\\`')
+    cmd = cmd.replace('"', '\\"')
+    pre_cmd = "touch /root/myrc && source /root/myrc"
+    cmd = f"""bash -c "{pre_cmd} && {cmd}" """
+    return shlex.split(cmd.strip())
+
+
+def check_exists(cmd: str):
+    prep_cmd = prepare_cmd(cmd)
+    try:
+        out, err, code = remoto.process.check(conn, prep_cmd)
+    except Exception as e:
+        print(e)
+        return False
+    print(out, err)
+    return code == 0
+
+
+def run(cmd: str):
+    prep_cmd = prepare_cmd(cmd)
+    print(prep_cmd)
+    try:
+        remoto.process.run(conn, prep_cmd)
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    tools = [Path, Poetry, Rust, Just, Repo]
+    conn = remoto.Connection("linode")
+    for tool in tools:
+        if not check_exists(tool.exist_cmd()):
+            run(tool.install_cmd())
