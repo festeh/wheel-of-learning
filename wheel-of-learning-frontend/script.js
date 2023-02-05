300939bb400958ebc972ca7e4af8896889c14554
Dima Lipin <dimich3d@ya.ru>
2023-02-05 17:51:51 +0100

    upd

--- wheel-of-learning-frontend/script.js
+++ wheel-of-learning-frontend/script.js
@@ -1,12 +1,35 @@
-function getTasks() {
-    fetch('/api/tasks')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
+async function getTasks() {
+  const response = await fetch(`/api/tasks`);
+  if (response.status !== 200) {
+    throw new Error(response.statusText);
+  }
+  const tasks = await response.json();
+  return tasks;
+}
+//
+function renderTasks() {
+  const tasksElem = document.getElementById("task-list");
+  tasksElem.innerHTML = "";
+  getTasks()
+    .then((tasks) => {
+      for (let task of tasks) {
+        const taskElem = document.createElement("div");
+        const taskText = document.createElement("span")
+        taskElem.className = "task-item";
+        taskText.textContent = task;
+        taskElem.appendChild(taskText);
+        tasksElem.appendChild(taskElem);
+      }
+      //     table.innerHTML = '';
+      //     tasks.forEach(task => {
+      //         const tr = document.createElement('tr');
+      //         const td = document.createElement('td');
+      //         td.innerHTML = task.title;
+    })
+    .catch((error) => {
+      tasksElem.innerText = error.message;
+      console.log("Error", error);
+    });
 }
 
-getTasks();
+renderTasks();
